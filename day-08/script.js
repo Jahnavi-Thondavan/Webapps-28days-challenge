85fafdb05c2724c36133e8fd701353f518f7059f
janu <yourgithubemail@gmail.com>
2026-01-30 20:39:38 +0530

    Arrays

---
+++ day-08/script.js
@@ -0,0 +1,17 @@
+let arr=[10,20,30,40];
+console.log (arr);
+let len=arr.length;
+console.log(len);
+console.log(arr[0]);
+console.log(arr[3]);
+arr[0]=90;
+console.log(arr);
+console.log (arr[0]);
+delete arr[0];
+console.log(arr);
+arr.push(80);
+console.log(arr);
+let arrey=["Aaron","Ella","Kenji","Nazeera","Paris","Delulu"];
+console.log(arrey);
+
+// DYNAMIC RENDARENIN
