115cd8546f3530172ab23506c1f2aec580cb7bcb
Jahnavi-Thondavan <thondavanjahnavi@gmail.com>
2026-02-23 20:27:53 +0530

    Day 16

---
+++ day-16/my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Comp from "./comp.jsx"
+import "./app.css"
+
+function App() {
+  return (
+    <>
+      <Comp />
+      <Comp />
+      <Comp />
+      <Comp />
+    </>
+  )
+}
+
+export default App
---
+++ day-16/my-project/src/comp.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./comp.css"
+
+function Comp() {
+  return (
+    <div className="card">
+      <h1 className="h1">Hello Everyone!!</h1>
+      <p className="p">My name is <b><em>Jahnavi Thondavan</em></b> and I'm a tech entusiast</p>
+    </div>
+  )
+}
+
+export default Comp
