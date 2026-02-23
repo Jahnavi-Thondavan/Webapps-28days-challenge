d7e0ff9717077c333c2d0e5dfa2781111a6ed85f
Jahnavi-Thondavan <thondavanjahnavi@gmail.com>
2026-02-23 20:49:36 +0530

    React Basics and Components

--- day-16/src/comp.jsx
+++ day-16/src/comp.jsx
@@ -4,7 +4,7 @@ import "./comp.css"
 function Comp() {
   return (
     <div className="card">
-      <h1 className="h1">Hello Everyone!!</h1>
+      <h1 className="h1">Hello Everyone!</h1>
       <p className="p">My name is <b><em>Jahnavi Thondavan</em></b> and I'm a tech entusiast</p>
     </div>
   )
